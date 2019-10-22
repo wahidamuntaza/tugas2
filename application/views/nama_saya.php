9fa7fbda7058cefc342786acd87231e248cf6958
wahidamuntaza <wahidamuntazasuhadi123@gamil.com>
2019-10-22 15:01:25 +0700

    tugas2

---
+++ application/views/nama_saya.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>PRAKTIKUM<h1>
+<p>nama saya adalah daniel</p>
+</body>
+</html>
